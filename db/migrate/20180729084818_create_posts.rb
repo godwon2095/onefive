40be8f73ff9f5652ad78485b7483b77d91f78e72
seongwonJang <jsw2095@naver.com>
2018-08-03 05:40:57 +0000

    CRUD구상중

--- db/migrate/20180729084818_create_posts.rb
+++ db/migrate/20180729084818_create_posts.rb
@@ -2,7 +2,10 @@ class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
       t.string :title
+      t.text :subtitle
       t.text :content
+      t.string :music_images, array: true, :default => []
+      t.string :music_titles, array: true, :default => []
       t.belongs_to :user
       t.integer :view_count, default: 0
 
--- db/schema.rb
+++ db/schema.rb
@@ -75,11 +75,14 @@ ActiveRecord::Schema.define(version: 20180731074237) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
+    t.text     "subtitle"
     t.text     "content"
+    t.string   "music_images", default: "--- []\n"
+    t.string   "music_titles", default: "--- []\n"
     t.integer  "user_id"
-    t.integer  "view_count", default: 0
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.integer  "view_count",   default: 0
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
   end
 
   create_table "users", force: :cascade do |t|
