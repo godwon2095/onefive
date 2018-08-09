723bfb34941a9b62fa24839000f037d715b1183b
sungwon <jsw2095@naver.com>
2018-08-09 21:15:44 +0900

    crawl going

--- app/models/singer.rb
+++ app/models/singer.rb
@@ -2,19 +2,25 @@ class Singer < ActiveRecord::Base
   has_many :songs
 
   def self.generate_singers
-    (1..500000).each do |i|
+    (340000..500000).each do |i|
       result = Wombat.crawl do
 
         base_url "https://music.naver.com/"
         path "/artist/track.nhn?artistId=" + "#{i}" + "&sorting=popular" #아티스트별
-        music_singers({ css: ".tb_artist"  }, :list)
+        byebug
 
-          features css: '.nav-item-opensource'
-          business css: '.nav-item-business'
+          music_singers({ css: ".tb_artist"  }, :list)
+
+            features css: '.nav-item-opensource'
+            business css: '.nav-item-business'
       end
 
-      result["music_singers"].each_with_index do |s, index|
-          Singer.find_or_create_by(name: s)
+      if result.present?
+        result["music_singers"].each_with_index do |s, index|
+          if Singer.find_by(name: s).nil?
+            Singer.find_or_create_by(name: s)
+          end
+        end
       end
     end
   end
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,7 +2,7 @@ class Song < ActiveRecord::Base
   belongs_to :singer
 
   def self.generate_songs
-    (1..500000).each do |i|
+    (150..500000).each do |i|
       result = Wombat.crawl do
 
         base_url "https://music.naver.com/"
@@ -16,11 +16,21 @@ class Song < ActiveRecord::Base
           business css: '.nav-item-business'
       end
 
-      result["music_titles"].each_with_index do |r, index|
-          Song.find_or_create_by(title: r,
-                                 singer_id: Singer.find_by(name: result["music_singers"][index]).id,
-                                 image: result["music_images"][index],
-                                 album: result["music_albums"][index])
+      if result.present?
+        result["music_titles"].each_with_index do |r, index|
+            if Singer.find_by(name: result["music_singers"]).present?
+              if Song.find_or_create_by(title: r,
+                                     singer_id: Singer.find_by(name: result["music_singers"][index]).id,
+                                     image: result["music_images"][index],
+                                     album: result["music_albums"][index]).nil?
+
+                Song.find_or_create_by(title: r,
+                                       singer_id: Singer.find_by(name: result["music_singers"][index]).id,
+                                       image: result["music_images"][index],
+                                       album: result["music_albums"][index])
+             end
+            end
+        end
       end
     end
   end
