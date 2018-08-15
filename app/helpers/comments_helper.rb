b0e292a131655cf0f8d0fa55dc161d728d4f828d
sungwon <jsw2095@naver.com>
2018-08-15 17:39:13 +0900

    user tag 기능 완성

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,5 @@
 module CommentsHelper
+  def render_with_hashtags(content)
+    content.gsub(/@\w+/){|word| link_to word, "/timelines/tag/#{word.delete('@')}"}.html_safe
+  end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,7 @@
 class Tag < ActiveRecord::Base
   belongs_to :user
+
+  def generate_alarms
+    
+  end
 end
