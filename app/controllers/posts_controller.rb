1c90607ac42fefd68d221ac7068e2629a53a3d8e
sungwon <jsw2095@naver.com>
2018-08-16 16:15:05 +0900

    infinite scroll구현 및 render partial로 코드 정리

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,9 +5,9 @@ class PostsController < ApplicationController
 
   def index
     if params[:search]
-      @posts = Post.search(params[:search]).paginate(:page => params[:page], :per_page => 3)
+      @posts = Post.search(params[:search]).order(created_at: :desc).paginate(:page => params[:page], :per_page => 3)
     elsif params[:search_user]
-      @posts = User.search(params[:search_user]).find_posts.paginate(:page => params[:page], :per_page => 3)
+      @posts = User.search(params[:search_user]).find_posts.order(created_at: :desc).paginate(:page => params[:page], :per_page => 3)
     else
       @posts = Post.order(created_at: :desc).paginate(:page => params[:page], :per_page => 3)
     end
