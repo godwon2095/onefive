29579bdbd0517adf1f18951ca5ec5e36c2f76ac5
sungwon <jsw2095@naver.com>
2018-08-18 20:09:52 +0900

    music 100개 제한

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,10 +19,10 @@ class PostsController < ApplicationController
 
   def search
     if params[:search_music]
-      @musics = Song.search(params[:search_music])
+      @musics = Song.search(params[:search_music]).paginate(:page => params[:page], :per_page => 100)
       @check_params = params[:search_music]
     elsif params[:search_singer]
-      @musics = Song.find_songs(Singer.search(params[:search_singer]))
+      @musics = Song.find_songs(Singer.search(params[:search_singer])).paginate(:page => params[:page], :per_page => 100)
     end
 
     respond_to do |format|
--- app/models/singer.rb
+++ app/models/singer.rb
@@ -2,11 +2,11 @@ class Singer < ActiveRecord::Base
   has_many :songs
 
   def self.generate_singers
-    # (340000..500000).each do |i|
+    # (114257..120000).each do |i|
       result = Wombat.crawl do
 
         base_url "https://music.naver.com/"
-        path "/artist/track.nhn?artistId=" + "112579" + "&sorting=popular" #아티스트별
+        path "/artist/track.nhn?artistId=" + "460475" + "&sorting=popular" #아티스트별
 
           music_singers({ css: ".tb_artist"  }, :list)
 
@@ -18,6 +18,7 @@ class Singer < ActiveRecord::Base
         result["music_singers"].each_with_index do |s, index|
           if Singer.find_by(name: s).nil?
             Singer.find_or_create_by(name: s)
+            puts i
           end
         end
       end
