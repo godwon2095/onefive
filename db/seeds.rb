055954dcbe3319c4f10cb1d75b07b0a703560277
Hyejin Lee <kuforest93@gmail.com>
2018-08-18 08:36:34 +0000

    체크박스 수정완료

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-if AdminUser.nil?
+# if AdminUser.nil?
   AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
-end
+# end
 # 50.times do Post.create(user_id: 1, title: "hello", subtitle: "hello2", content: "wawafsdfdsfdsfs")
 # end
-Singer.generate_singers
-Song.generate_songs
+# Singer.generate_singers
+# Song.generate_songs
