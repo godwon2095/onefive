dfa8681a180478d2e015ab0806161ec009e23ef2
sungwon <jsw2095@naver.com>
2018-08-21 22:34:04 +0900

    sign-up page front

--- app/models/subcomment.rb
+++ app/models/subcomment.rb
@@ -1,7 +1,15 @@
 class Subcomment < ActiveRecord::Base
+  after_create :generate_alarm
+
   belongs_to :user
   belongs_to :post
   belongs_to :comment
 
   has_many :tags
+
+  def generate_alarm
+    if self.comment.user.subcomment_on == true
+      Alarm.create(content: self.user.name, user_id: self.comment.user, findable_id: self.post.id, is_read: false, alarm_type: "subcomment")
+    end
+  end
 end
