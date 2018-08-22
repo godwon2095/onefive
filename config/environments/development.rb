db02df905bc0a41dafad44c770ff2eb0e1f1d795
sungwon <jsw2095@naver.com>
2018-08-22 20:42:46 +0900

    이메일 인증기능 도입 및 도메인 주소로 기본값 설정

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
   # devise : 이메일 인증 설정
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: 'http://localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'http://1!05.am' }
   ActionMailer::Base.smtp_settings = {
     :address              => 'smtp.gmail.com',
     :domain               => 'mail.google.com',
@@ -17,7 +17,7 @@ Rails.application.configure do
     :authentication       => 'login',
     :enable_starttls_auto => true
   }
-  
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
